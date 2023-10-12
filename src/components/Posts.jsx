77bd82581a841fbd9ee4e9603c168227f31e40ee
Dwayna Harris <dwaynaharris@gmail.com>
2023-10-12 09:28:00 -0400

    trying to  make both a GET and a POST request

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,11 +1,25 @@
 //Form for the posts of journal entries
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Posts() {
   const [firstName, setFirstName] = useState("first name");
   const [lastName, setLastName] = useState("last name");
   const [text, setText] = useState("Dear diary...");
   const [submittedData, setSubmittedData] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async() => {
+      const url = "http://localhost:4000/Posts";
+      try {
+        const response = await fetch(url);
+        const jsonData = await response.json();
+        console.log(jsonData);
+        return jsonData;
+      } catch(error){
+
+      }
+    }
+  }, []);
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -32,6 +46,7 @@ function Posts() {
   const listOfSubmissions = submittedData.map((data, index) => {
     return (
       <div key={index}>
+        <br></br>
         {data.firstName} {data.lastName} 
         <br></br>
         {data.text}
