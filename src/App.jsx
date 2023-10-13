2dc2fd9b31d5cb38f9626d56da3b9230b0735327
Dwayna Harris <dwaynaharris@gmail.com>
2023-10-12 20:47:59 -0400

    making client side routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import Header from './components/Header'
 import Posts from './components/Posts'
 import Footer from './components/Footer'
+import { HashRouter } from 'react-router-dom'
 
 
 function App() {
 
   return (
     <>
-      <Header />
+      <HashRouter>
+        <Header />
+      </HashRouter>
       <Posts />
       <Footer />
     </>
